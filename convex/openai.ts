3b7cbc918857b00562108764290c47ff63b16951
misty <145301341+yessineZ@users.noreply.github.com>
2024-10-05 05:51:50 -1200

    Update openai.ts

--- convex/openai.ts
+++ convex/openai.ts
@@ -33,7 +33,76 @@ export const mistyRobot = action({
             messageContent = res.choices[0].message.content;
         } catch (error) {
             console.error('OpenAI API error:', error);
-            messageContent = "idk brother xd" ; 
+            messageContent = "import OpenAi from 'openai';
+import { action } from './_generated/server';
+import { v } from 'convex/values';
+import { api } from './_generated/api';
+
+const apiKey =process.env.OPENAI_API_KEY ; 
+
+const openAi = new OpenAi({ apiKey });
+
+export const mistyRobot = action({
+
+    args: {
+        messageBody: v.string(),
+        conversation: v.id('conversations'),
+    },
+    handler: async (ctx, args) => {
+        let messageContent;
+        console.log(args.messageBody + apiKey);
+        try {
+            const res = await openAi.chat.completions.create({
+                model: 'gpt-3.5-turbo',
+                messages: [
+                    {
+                        role: 'system',
+                        content: 'You are a terse bot and you are in a group chat responding to questions with 1-sentence answers.',
+                    },
+                    {
+                        role: 'user',
+                        content: args.messageBody,
+                    },
+                ],
+            });
+            messageContent = res.choices[0].message.content;
+        } catch (error) {
+            console.error('OpenAI API error:', error);
+            messageContent = "JavaScript is a high-level, dynamic programming language primarily used to create interactive and dynamic content on websites" ; 
+        }
+
+        await ctx.runMutation(api.messages.sendChatGPTMessage, {
+            content: messageContent! ,
+            conversation: args.conversation,
+            messageType: 'text',
+        });
+    },
+});
+
+
+
+
+export const mistyRobot2 = action({
+    args : {
+        messageBody : v.string(),
+        conversation : v.id('conversations'),
+    },
+    handler : async (ctx , args) => {
+        const res = await openAi.images.generate({
+            model : 'dall-e-2'  ,
+            prompt : args.messageBody,
+            n : 1,
+            size : '512x512',
+        });
+        const imageUrl = res.data[0].url;
+        await ctx.runMutation(api.messages.sendChatGPTMessage, {
+            content: imageUrl ?? '/mistyRobot.png' ,
+            conversation: args.conversation,
+            messageType: 'image',
+        });
+
+    }
+})" ; 
         }
 
         await ctx.runMutation(api.messages.sendChatGPTMessage, {
